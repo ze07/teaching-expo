bee638c75f58c0f174677df25e49a04d3f5f5315
zineb djihane <137612486+ze07@users.noreply.github.com>
2025-11-14 15:37:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Teaching Expo website loaded');
+// Simple smooth scroll for nav links
+document.querySelectorAll("a[href^='#']").forEach(link =>
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  })
+);
